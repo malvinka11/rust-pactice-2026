18711208f558bbf03f58f7b5362f46e28541e311
malvinka11 <viktoria.matvienko@e-u.edu.ua>
2026-03-09 22:48:16 +0100

    practice 3 staircase

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,1 +1,24 @@
-// https://www.hackerrank.com/challenges/staircase/problem
+pub fn staircase(n: i32) -> Vec<String> {
+    let mut result = Vec::new();
+    for i in 1..=n {
+        let spaces = " ".repeat((n - i) as usize);
+        let hashes = "#".repeat(i as usize);
+        result.push(format!("{}{}", spaces, hashes));
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_staircase() {
+        let expected = vec![
+            "  #".to_string(),
+            " ##".to_string(),
+            "###".to_string(),
+        ];
+        assert_eq!(staircase(3), expected);
+    }
+}
