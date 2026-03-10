c341a6b3921b9298725c929eca1c24d8a2149747
malvinka11 <viktoria.matvienko@e-u.edu.ua>
2026-03-10 01:38:27 +0100

    practice 10 sock merchant

--- src/hackerrank/task10.rs
+++ src/hackerrank/task10.rs
@@ -1,1 +1,22 @@
-// https://www.hackerrank.com/challenges/sock-merchant/problem
+use std::collections::HashMap;
+
+pub fn sock_merchant(ar: Vec<i32>) -> i32 {
+    let mut map = HashMap::new();
+
+    for sock in ar {
+        *map.entry(sock).or_insert(0) += 1;
+    }
+
+    map.values().map(|v| v / 2).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sock_merchant() {
+        let arr = vec![10, 20, 20, 10, 10, 30, 50, 10, 20];
+        assert_eq!(sock_merchant(arr), 3);
+    }
+}
