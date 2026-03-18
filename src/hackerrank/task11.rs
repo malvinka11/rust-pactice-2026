c3b7ec899729420b3536bb7264f7a318b0bdfc6f
malvinka11 <viktoria.matvienko@e-u.edu.ua>
2026-03-18 04:04:58 +0100

    practice 11

---
+++ src/hackerrank/task11.rs
@@ -0,0 +1,27 @@
+pub fn diagonal_difference(arr: Vec<Vec<i32>>) -> i32 {
+    let n = arr.len();
+    let mut primary = 0;
+    let mut secondary = 0;
+
+    for i in 0..n {
+        primary += arr[i][i];
+        secondary += arr[i][n - 1 - i];
+    }
+
+    (primary - secondary).abs()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_diagonal_difference() {
+        let arr = vec![
+            vec![11, 2, 4],
+            vec![4, 5, 6],
+            vec![10, 8, -12],
+        ];
+        assert_eq!(diagonal_difference(arr), 15);
+    }
+}
